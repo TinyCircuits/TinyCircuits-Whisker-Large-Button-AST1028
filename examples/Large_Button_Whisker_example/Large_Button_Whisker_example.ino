2921751095712612529dfdf1210c6ccd892bc046
TinyCircuits <info@tinycircuits.com>
2019-07-30 15:21:07 -0400

    cleaned up code

--- examples/Large_Button_Whisker_example/Large_Button_Whisker_example.ino
+++ examples/Large_Button_Whisker_example/Large_Button_Whisker_example.ino
@@ -7,30 +7,16 @@
 // Written by Zachary Lee 7/30/19
 //************************************************************************
 
-#include <Wire.h>               //I2C Library 
-//#include "TinyBoards.h"       //TinyBoards Library - contains default device addresses and useful functions
-
-const int powerPin = 4;         //IO Pin for controlling power to the Mux TinyShield
-
-//const int buttonPin =
-
-float buttonPin = A0;
+#define buttonPin A0 // corresponds to Port 0. Similarly, Port 1 = A1, Port 2 = A2, Port 3 = A3.
 
 void setup() {
   Wire.begin();                 //Begin I2C Communication
   SerialUSB.begin(9600);         //Begin Serial Communication and set Baud Rate
 
-  pinMode(powerPin, OUTPUT);
-  digitalWrite(powerPin, HIGH);
-
   pinMode(buttonPin, INPUT);
-
-  selectPort(0);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  selectPort(1);
   static int buttonState = 0;
   int prevButtonState = buttonState;
   buttonState = digitalRead(buttonPin);   //Poll Sensor Data
@@ -46,10 +32,4 @@ void loop() {
       // button not currently being pressed
     }
   }
-}
-
-void selectPort(int port) {
-  Wire.beginTransmission(0x70);   //I2C Multiplexer Address
-  Wire.write(0x04 + port);        //Mux Port Select (0 to 3)
-  Wire.endTransmission();         //End Communication with Mux
 }
