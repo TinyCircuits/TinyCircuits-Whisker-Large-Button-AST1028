0370749e9a082a67890103a42320b0ad322aa1d2
Rena <info@tinycircuits.com>
2019-12-04 14:57:35 -0500

    code

--- examples/Large_Button_Wireling_example/Large_Button_Wireling_example.ino
+++ examples/Large_Button_Wireling_example/Large_Button_Wireling_example.ino
@@ -3,10 +3,15 @@
  * Uses digitalRead to detect when the large button is pressed and report  
  * state to the Serial Monitor
  * 
- * Written by Zachary Lee 7/30/19
+ * Hardware by: TinyCircuits
+ * Written by: Zachary Lee for TinyCircuits
+ * 
+ * Initiated: 07/25/2019
+ * Last Updated: 12/04/2019 
  *************************************************************************/
 
 #include <Wire.h>
+#include <Wireling.h> // For interfacing with Wirelings
 
 #define buttonPin A0 // corresponds to Port 0. Similarly, Port 1 = A1, Port 2 = A2, Port 3 = A3.
 
@@ -17,18 +22,20 @@
   #define SerialMonitorInterface SerialUSB
 #endif
 
-
 void setup() {
   Wire.begin();                         // Begin I2C Communication
   SerialMonitorInterface.begin(9600);   // Begin Serial Communication and set Baud Rate
 
-  pinMode(buttonPin, INPUT);
+  // Enable & Power Wireling
+  Wireling.begin();
+
+  pinMode(buttonPin, INPUT); // The button is an input 
 }
 
 void loop() {
   static int buttonState = 0;
   int prevButtonState = buttonState;
-  buttonState = digitalRead(buttonPin);   //Poll Sensor Data
+  buttonState = digitalRead(buttonPin);   // Poll Sensor Data
 
   if (prevButtonState != buttonState) // no need to alert the user if button state remains unchanged
   {
